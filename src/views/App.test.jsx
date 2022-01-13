6f58a250a1c6e6c272c89f35de60986f35ba87e0
Julius Franklin <jjfranklin88@outlook.com>
2022-01-13 14:16:28 -0500

    fixing logic to add slice, test passing

--- src/views/App.test.jsx
+++ src/views/App.test.jsx
@@ -21,12 +21,42 @@ describe('app behavior test', () => {
         fireEvent.change(dateInput, { target: { value: '2022-01-14'}})
         screen.getByText('2022-01-14', { exact: false })
 
+        fireEvent.change(dateInput, { target: { value: '2022-01-16'}})
+        screen.getByText('2022-01-16', { exact: false })
+
+
 
         fireEvent.click(undoButton)
-        screen.getByText('2022-01-13')
+        screen.getByText('2022-01-14')
 
         fireEvent.click(redoButton)
+        screen.getByText('2022-01-16')
+
+        fireEvent.click(undoButton)
         screen.getByText('2022-01-14')
+
+        fireEvent.change(dateInput, { target: { value: '2022-01-15'}})
+        screen.getByText('2022-01-16', { exact: false })
+
+        fireEvent.click(undoButton)
+        screen.getByText('2022-01-15')
+
+        fireEvent.click(redoButton)
+        screen.getByText('2022-01-16')
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     })
